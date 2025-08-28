0b9af5439fc3f4339455849c347bf12b81500026
Winy <danielamanzanorangel@gmail.com>
2025-08-28 13:26:52 -0600

    xx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,15 @@ export class AppComponent {
   constructor(private primeng: PrimeNG) {}
 
   ngOnInit() {
-      this.primeng.ripple.set(true);
+    this.primeng.ripple.set(true);
+    const loader = document.getElementById('loader');
+    if (loader) {
+      loader.classList.add('loader-hidden');
+
+      loader.addEventListener('transitionend', () => {
+        loader.remove();
+      });
+    }
   }
   title = 'my-portfolio';
 }
